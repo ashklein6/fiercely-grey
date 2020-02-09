a4c28c66684eeb9fd4096977ee134119192d63e2
Ashley Klein <ashklein6@gmail.com>
2020-02-09 11:49:06 -0600

    updated footer

--- wordpress/wp-content/themes/fiercelygrey/footer.php
+++ wordpress/wp-content/themes/fiercelygrey/footer.php
@@ -15,17 +15,48 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
+			<div class="footer-column">
+				<p>
+					© Fiercely Grey
+				</p>
+			</div>
+			<div class="footer-column">
+				<h4>INFORMATION</h4>
+				<ul>
+					<li>
+						<a href="">About</a>
+					</li>
+					<li>
+						<a href="">FAQ</a>
+					</li>
+					<li>
+						<a href="">Contact</a>
+					</li>
+				</ul>
+			</div>
+
+			<div class="footer-column">
+				<h4>FOLLOW</h4>
+				<ul>
+					<li>
+						<a href="https://www.instagram.com/fiercelygrey/">Instagram</a>
+					</li>
+					<li>
+						<a href="https://twitter.com/fiercelygrey">Twitter</a>
+					</li>
+					<li>
+						<a href="https://youtube.com/user/fiercelygrey">Youtube</a>
+					</li>
+				</ul>
+			</div>
+
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'fiercelygrey' ) ); ?>">
 				<?php
 				/* translators: %s: CMS name, i.e. WordPress. */
 				printf( esc_html__( 'Proudly powered by %s', 'fiercelygrey' ), 'WordPress' );
 				?>
 			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'fiercelygrey' ), 'fiercelygrey', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
